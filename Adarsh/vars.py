18458a2eb01be11f7223145e7261886d10cedf01
parshucr123 <118430657+parshucr123@users.noreply.github.com>
2023-12-04 17:07:45 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -36,5 +36,5 @@ class Var(object):
     else:
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', "mongodb+srv://ladykiller2:ladykiller2@cluster0.eezeovn.mongodb.net/?retryWrites=true&w=majority"))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'elitecraft_studios'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'pcnewmoviez'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
